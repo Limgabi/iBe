38fb030f2732aca41c2d1b1110e213bb3edca850
임가비 <gabi520@naver.com>
2026-02-09 08:45:49 +0900

    fix: GuideSection 너비 수정

--- src/components/GuideSection.tsx
+++ src/components/GuideSection.tsx
@@ -4,7 +4,7 @@ import heartImg from "../assets/images/heart.png";
 
 export default function GuideSection() {
   return (
-    <section className="flex flex-col gap-20 pt-15 pb-20 text-center items-center text-sm font-medium leading-[190%] text-[#454545]">
+    <section className="flex flex-col gap-20 pt-15 pb-20 text-center items-center text-sm font-medium leading-[190%] text-[#454545] w-full">
       <Image src={letterImg} width={122} height={32} alt="letter" />
       <div className="flex flex-col gap-5 items-center">
         <Image src={heartImg} width={32} height={32} alt="heart" />
