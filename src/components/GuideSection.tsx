28f969eb41d88dabf7710c7a84f6f291a9f6c14d
임가비 <gabi520@naver.com>
2026-02-09 09:59:55 +0900

    fix: GuideSection letter 이미지 제거

--- src/components/GuideSection.tsx
+++ src/components/GuideSection.tsx
@@ -1,24 +1,20 @@
 import Image from "next/image";
-import letterImg from "../assets/images/letter.png";
 import heartImg from "../assets/images/heart.png";
 
 export default function GuideSection() {
   return (
-    <section className="flex flex-col gap-20 pt-15 pb-20 text-center items-center text-sm font-medium leading-[190%] text-[#454545] w-full">
-      <Image src={letterImg} width={122} height={32} alt="letter" />
-      <div className="flex flex-col gap-5 items-center">
-        <Image src={heartImg} width={32} height={32} alt="heart" />
-        <div>
-          설날이에요! 🎉 <br />
-          2026년을 시작하는 당신을 위해
-          <br />
-          행운 뽑기가 방금 막 배달되었어요!
-          <br />
-          <br />
-          원하는 오너먼트를 눌러
-          <br />
-          행운을 받아보세요 ♡
-        </div>
+    <section className="flex flex-col gap-5 pb-20 text-center items-center text-sm font-medium leading-[190%] text-[#454545] w-full">
+      <Image src={heartImg} width={32} height={32} alt="heart" />
+      <div>
+        설날이에요! 🎉 <br />
+        2026년을 시작하는 당신을 위해
+        <br />
+        행운 뽑기가 방금 막 배달되었어요!
+        <br />
+        <br />
+        원하는 오너먼트를 눌러
+        <br />
+        행운을 받아보세요 ♡
       </div>
     </section>
   );
