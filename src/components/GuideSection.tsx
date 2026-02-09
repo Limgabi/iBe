fa7151fa0dfb62ea93fb63561ed30aeef403accf
임가비 <gabi520@naver.com>
2026-02-09 16:35:10 +0900

    fix: GuideSection 문구 수정

--- src/components/GuideSection.tsx
+++ src/components/GuideSection.tsx
@@ -1,12 +1,8 @@
-import Image from "next/image";
-import heartImg from "../assets/images/heart.png";
-
 export default function GuideSection() {
   return (
-    <section className="flex flex-col gap-5 pb-20 text-center items-center text-sm font-medium leading-[190%] text-[#454545] w-full">
-      <Image src={heartImg} width={32} height={32} alt="heart" />
+    <section className="flex flex-col gap-5 pb-18 text-center items-center text-sm font-medium leading-[190%] text-[#D87875] w-full">
       <div>
-        설날이에요! 🎉 <br />
+        설날이에요 🎉 <br />
         2026년을 시작하는 당신을 위해
         <br />
         행운 뽑기가 방금 막 배달되었어요!
@@ -14,7 +10,7 @@ export default function GuideSection() {
         <br />
         원하는 오너먼트를 눌러
         <br />
-        행운을 받아보세요 ♡
+        행운을 받아보세요 ♥
       </div>
     </section>
   );
