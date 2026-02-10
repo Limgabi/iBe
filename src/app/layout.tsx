2d233d4a662f672805cfbf4b24a51cddf1eb0430
임가비 <gabi520@naver.com>
2026-02-10 13:04:49 +0900

    chore: 오픈그래프 정보 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,17 @@ import KakaoScript from "@/src/components/KakaoScript";
 export const metadata: Metadata = {
   title: "iBe",
   description: "아이비의 행운 뽑기",
+  openGraph: {
+    type: "website",
+    locale: "ko_KR",
+    title: "iBe",
+    description: "아이비의 행운 뽑기",
+    images: [
+      {
+        url: "/og-card.png",
+      },
+    ],
+  },
   verification: {
     google: process.env.GOOGLE_SITE_VERIFICATION,
   },
