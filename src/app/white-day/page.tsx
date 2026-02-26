b67cb00db17be6f9380c94ce84ac3f65e498d061
gabi <70315572+Limgabi@users.noreply.github.com>
2026-02-26 20:21:00 +0900

    feat: 화이트데이 메인 페이지 - 이미지 (#67)

--- src/app/white-day/page.tsx
+++ src/app/white-day/page.tsx
@@ -1,8 +1,9 @@
-import { meongiB } from "@/src/app/fonts";
+import { meongiB } from '@/src/app/fonts';
+import Icon from '@/src/components/common/icon/icon';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[#030303]">
+    <div className="relative flex min-h-screen items-center justify-center bg-[#030303]">
       <main className="flex min-h-screen w-full max-w-97.5 flex-col items-center bg-[#FFFBE8] sm:items-start">
         <h1
           className={`${meongiB.className} text-main-title select-none text-[#B5644E] w-full pt-25 text-center`}
@@ -11,6 +12,39 @@ export default function Home() {
           <br />
           Letter
         </h1>
+
+        <Icon
+          name="heartPrimary"
+          width={40}
+          height={38}
+          style={{
+            position: 'absolute',
+            top: 214,
+            left: '50%',
+            transform: 'translateX(-50%)',
+          }}
+        />
+        <Icon
+          name="heartSecondary"
+          width={40}
+          height={38}
+          style={{
+            position: 'absolute',
+            top: 269,
+            right: '35%',
+          }}
+        />
+        <Icon
+          name="candy"
+          width={281}
+          height={261}
+          style={{
+            position: 'absolute',
+            top: 288,
+            left: '50%',
+            transform: 'translateX(-50%)',
+          }}
+        />
       </main>
     </div>
   );
--- src/components/common/icon/icon-map.ts
+++ src/components/common/icon/icon-map.ts
@@ -1,16 +1,22 @@
-import Download from "../../../assets/icons/download.svg";
-import Inquiry from "../../../assets/icons/inquiry.svg";
-import Kakao from "../../../assets/icons/kakao.svg";
-import Rectangle from "../../../assets/icons/rectangle.svg";
-import Rectangle2 from "../../../assets/icons/rectangle-2.svg";
-import X from "../../../assets/icons/x.svg";
+import Candy from '../../../assets/icons/candy.svg';
+import Download from '../../../assets/icons/download.svg';
+import HeartPrimary from '../../../assets/icons/heart-primary.svg';
+import HeartSecondary from '../../../assets/icons/heart-secondary.svg';
+import Inquiry from '../../../assets/icons/inquiry.svg';
+import Kakao from '../../../assets/icons/kakao.svg';
+import Rectangle from '../../../assets/icons/rectangle.svg';
+import Rectangle2 from '../../../assets/icons/rectangle-2.svg';
+import X from '../../../assets/icons/x.svg';
 
 export const iconMap = {
+  candy: Candy,
   download: Download,
+  heartPrimary: HeartPrimary,
+  heartSecondary: HeartSecondary,
   inquiry: Inquiry,
   kakao: Kakao,
   rectangle: Rectangle,
-  "rectangle-2": Rectangle2,
+  'rectangle-2': Rectangle2,
   x: X,
 } as const;
 
