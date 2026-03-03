b2198baef2e23b274f564fe301d4f93a2d8a3875
임가비 <gabi520@naver.com>
2026-03-04 08:46:51 +0900

    feat: 서비스/버튼 텍스트 문구 수정 (#69)

--- src/app/white-day/page.tsx
+++ src/app/white-day/page.tsx
@@ -1,6 +1,6 @@
-import { meongiB } from '@/src/app/fonts';
-import Icon from '@/src/components/common/icon/icon';
-import InquiryButton from '@/src/components/common/inquiry/inquiry-button';
+import { meongiB } from "@/src/app/fonts";
+import Icon from "@/src/components/common/icon/icon";
+import InquiryButton from "@/src/components/common/inquiry/inquiry-button";
 
 export default function Home() {
   return (
@@ -20,39 +20,39 @@ export default function Home() {
             width={40}
             height={38}
             style={{
-              position: 'absolute',
+              position: "absolute",
               top: 12,
-              left: '50%',
-              transform: 'translateX(-50%)',
+              left: "50%",
+              transform: "translateX(-50%)",
             }}
           />
           <Icon
             name="heartSecondary"
             width={40}
             height={38}
-            style={{ position: 'absolute', top: 55, right: '25%' }}
+            style={{ position: "absolute", top: 55, right: "25%" }}
           />
           <Icon
             name="candy"
             width={281}
             height={261}
             style={{
-              position: 'absolute',
+              position: "absolute",
               top: 74,
-              left: '50%',
-              transform: 'translateX(-50%)',
+              left: "50%",
+              transform: "translateX(-50%)",
             }}
           />
         </div>
         <div className="w-full py-10 flex flex-col gap-7 items-center">
           <p className="font-semibold text-base leading-[160%] text-[#B5644E] text-center">
-            소중한 사람에게
+            그 사람을 떠올리며 성격을 고르면,
             <br />
-            달콤함을 선물해보세요!
+            어울리는 달콤한 선물과 편지가 완성돼요.
           </p>
 
           <button className="w-fit  flex gap-1 items-center text-[#ffffff] text-button px-6 py-3 rounded-[365px] bg-[#B5644E] cursor-pointer">
-            <span>선물 보내기</span>
+            <span>선물 만들기</span>
             <Icon name="gift" width={16} height={16} />
           </button>
         </div>
