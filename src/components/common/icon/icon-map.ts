1b3e75f26431b15f13c4155b84709edc3a6f861f
임가비 <gabi520@naver.com>
2026-03-04 13:43:22 +0900

    feat: arrow 아이콘 추가 (#72)

--- src/components/common/icon/icon-map.ts
+++ src/components/common/icon/icon-map.ts
@@ -1,3 +1,4 @@
+import ArrowRight from "../../../assets/icons/arrow-right.svg";
 import Download from "../../../assets/icons/download.svg";
 import Gift from "../../../assets/icons/gift.svg";
 import InquiryWhiteDay from "../../../assets/icons/inquiry-white-day.svg";
@@ -8,6 +9,7 @@ import Rectangle2 from "../../../assets/icons/rectangle-2.svg";
 import X from "../../../assets/icons/x.svg";
 
 export const iconMap = {
+  arrowRight: ArrowRight,
   download: Download,
   gift: Gift,
   inquiryWhiteDay: InquiryWhiteDay,
