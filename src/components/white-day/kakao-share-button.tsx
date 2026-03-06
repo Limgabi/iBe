1203dd6f98bb55cfdf9fdc16f6d66115feb5e930
임가비 <gabi520@naver.com>
2026-03-06 13:30:35 +0900

    feat: 화이트데이 선물 카카오톡 공유하기 버튼 (#85)

---
+++ src/components/white-day/kakao-share-button.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import Icon from "@/src/components/common/icon/icon";
+import { Gift } from "@/src/components/white-day/step-5";
+
+interface KakaoShareButtonProps {
+  giftId: string;
+  gift: Gift;
+}
+
+export default function KakaoShareButton({
+  giftId,
+  gift,
+}: KakaoShareButtonProps) {
+  const handleClickShare = () => {
+    if (!window.Kakao) return;
+
+    if (!window.Kakao.isInitialized()) return;
+
+    const targetUrl = `https://ibe-lucky.vercel.app/white-day/gift/${giftId}`;
+
+    window.Kakao.Share.sendDefault({
+      objectType: "feed",
+      content: {
+        title: "iBe - 행운 뽑기 | 선물이 도착했어요!",
+        description: `To. ${gift.receiver}\n${gift.title}`,
+        imageUrl: `https://ibe-lucky.vercel.app/white-day/og-card.png`,
+        imageWidth: 295,
+        imageHeight: 386,
+        link: {
+          mobileWebUrl: targetUrl,
+          webUrl: targetUrl,
+        },
+      },
+      buttons: [
+        {
+          title: "선물 받으러 가기",
+          link: {
+            mobileWebUrl: targetUrl,
+            webUrl: targetUrl,
+          },
+        },
+      ],
+    });
+  };
+
+  return (
+    <button
+      onClick={handleClickShare}
+      className="border-none outline-none cursor-pointer"
+    >
+      <Icon name="whiteDayKakao" width={222} height={48} />
+    </button>
+  );
+}
