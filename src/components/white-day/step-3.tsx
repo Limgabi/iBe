4a95e3c7778a2d700d60f435ee0ebddb38b8b83d
gabi <70315572+Limgabi@users.noreply.github.com>
2026-03-08 11:39:33 +0900

    fix: 화이트데이 - 선물 받는 사람과 보내는 사람 텍스트 대괄호 제거 (#91)

--- src/components/white-day/step-3.tsx
+++ src/components/white-day/step-3.tsx
@@ -1,24 +1,24 @@
-import { useWhiteDayContext } from "@/src/contexts/white-day";
-import Button from "../common/button/button";
-import Icon from "../common/icon/icon";
-import { useRouter } from "next/navigation";
+import { useWhiteDayContext } from '@/src/contexts/white-day';
+import Button from '../common/button/button';
+import Icon from '../common/icon/icon';
+import { useRouter } from 'next/navigation';
 
 export default function Step3() {
   const router = useRouter();
 
   const { receiver, result } = useWhiteDayContext();
 
-  const dessertType = result?.title.split(" ")[0];
+  const dessertType = result?.title.split(' ')[0];
 
   const handleClickNext = () => {
-    router.push("/white-day/gift/new?step=4");
+    router.push('/white-day/gift/new?step=4');
   };
 
   return (
     <div className="flex flex-col justify-between items-center flex-1 h-full min-h-0 gap-15 overflow-y-auto">
       <div className="flex flex-col gap-12 w-full items-center">
         <p className="text-2xl text-center font-bold leading-[130%] tracking-[-0.02em] text-[#B5644E]">
-          [{receiver}]님은
+          {receiver}님은
           <br />
           <span className="text-[#E47F26]">{dessertType}</span> 같은 사람
         </p>
