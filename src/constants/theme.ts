ee517d06f3fa0377e3a440734868839daa759701
gabi <70315572+Limgabi@users.noreply.github.com>
2026-03-08 16:11:10 +0900

    fix: 문의하기 theme 색상 수정 (#93)

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,33 +1,37 @@
 export const THEMES = {
-  NEW_YEAR: "new-year",
-  WHITE_DAY: "white-day",
+  NEW_YEAR: 'new-year',
+  WHITE_DAY: 'white-day',
 } as const;
 
 export type ThemeKey = (typeof THEMES)[keyof typeof THEMES];
 
 export const THEME_UI = {
-  "new-year": {
+  'new-year': {
     inquiry: {
-      primary: "#EA706C",
-      primarySoft: "#FE8682",
-      disabled: "#BDBDBD",
-      outline: "#FBB4B2",
+      iconButton: '#EA706C',
+      textButton: '#EA706C',
+      primary: '#EA706C',
+      primarySoft: '#FE8682',
+      disabled: '#BDBDBD',
+      outline: '#FBB4B2',
     },
     steps: {
-      active: "#EA706C",
-      inactive: "#FE8682",
+      active: '#EA706C',
+      inactive: '#FE8682',
     },
   },
-  "white-day": {
+  'white-day': {
     inquiry: {
-      primary: "#F8DCC4",
-      primarySoft: "#6ED3C4",
-      disabled: "#BDBDBD",
-      outline: "#F8DCC4",
+      iconButton: '#F8DCC4',
+      textButton: '#B5644E',
+      primary: '#B5644E',
+      primarySoft: '#CF7C66',
+      disabled: '#BDBDBD',
+      outline: '#EFA895',
     },
     steps: {
-      active: "#CD8572",
-      inactive: "#F0DDBD",
+      active: '#CD8572',
+      inactive: '#F0DDBD',
     },
   },
 };
