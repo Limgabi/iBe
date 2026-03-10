04eadc2cead8921a8e239642cf292ebca7a42e80
임가비 <gabi520@naver.com>
2026-03-10 11:13:49 +0900

    style: GiftCard 너비 지정 (#103)

--- src/components/white-day/gift-detail-view.tsx
+++ src/components/white-day/gift-detail-view.tsx
@@ -34,6 +34,7 @@ export default function GiftDetailView({ gift }: GiftDetailViewProps) {
         </p>
 
         <motion.div
+          className="w-full"
           initial={{ opacity: 0, scale: 0.96 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ delay: 0.12, duration: 0.35 }}
