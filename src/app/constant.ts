d502054960d15996be3c739e5741ca83065371cc
임가비 <gabi520@naver.com>
2026-03-19 16:33:31 +0900

    feat: 메인페이지 UI 구현 (#152)

---
+++ src/app/constant.ts
@@ -0,0 +1,34 @@
+type ThemeService = {
+  link?: string;
+  title?: string;
+  description?: string;
+  bgColor?: string;
+  textColor?: string;
+};
+
+export const THEME_SERVICES: ThemeService[] = [
+  {},
+  {
+    link: "/new-year",
+    title: "LUCKY DRAW",
+    description: "설날 맞이 행운 뽑기",
+    bgColor: "#FFF2EC",
+    textColor: "#EA706C",
+  },
+  {
+    link: "/white-day",
+    title: "Sweet Letter",
+    description: "화이트데이 사탕 & 편지",
+    bgColor: "#FFFBE8",
+    textColor: "#B5644E",
+  },
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,52 @@
-import { redirect } from 'next/navigation';
+import { THEME_SERVICES } from "@/src/app/constant";
+import { meongiB } from "@/src/app/fonts";
+import InquiryButton from "@/src/components/common/inquiry/inquiry-button";
+import Link from "next/link";
 
 export default function Page() {
-  redirect('/white-day');
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-[#030303]">
+      <main className="flex min-h-screen w-full max-w-97.5 flex-col items-center bg-[#FDFFFC] sm:items-start">
+        <div className="py-15 px-5 w-full flex flex-col gap-8">
+          <h1
+            className={`${meongiB.className} text-center text-[50px] leading-[180%] tracking-[-0.03em] text-[#8ADCA0] select-none`}
+          >
+            iBe-Lucky
+          </h1>
+          <div className="flex gap-3 flex-wrap">
+            {THEME_SERVICES.map((theme, idx) => (
+              <div
+                key={idx}
+                className="h-20 w-[calc(50%-6px)] rounded-sm bg-white shadow-[0_0_12px_0_rgba(0,0,0,0.1)] items-center flex"
+              >
+                {theme.link ? (
+                  <Link
+                    href={theme.link}
+                    className="flex flex-col items-center w-full h-full justify-center"
+                    style={{
+                      color: theme.textColor,
+                      backgroundColor: theme.bgColor,
+                    }}
+                  >
+                    <span className={`${meongiB.className} text-lg`}>
+                      {theme.title}
+                    </span>
+                    <span className="leading-[160%] text-[14px] font-medium">
+                      {theme.description}
+                    </span>
+                  </Link>
+                ) : (
+                  <span className="text-center w-full font-medium text-[14px] leading-[160%] text-[#999999] cursor-not-allowed">
+                    {idx + 1}월<br />
+                    서비스 준비 중
+                  </span>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+        <InquiryButton />
+      </main>
+    </div>
+  );
 }
